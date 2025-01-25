a6a5e0033c35334cdf8c268328901e4cd47f847d
ikhwanhsn <ikhwanulhusna111@gmail.com>
2025-01-25 11:10:00 +0700

    chore: add call function to contract

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
 "use client";
 
 import { abi } from "@/services/abi";
-import { useAccount, useReadContract, useSignMessage } from "wagmi";
+import { parseEther } from "ethers";
+import { useAccount, useSignMessage } from "wagmi";
+import { useWriteContract } from "wagmi";
 
 export default function Home() {
-  const contractAddress = "0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8";
+  const contractAddress = "0x2335Ad4f04a4fD50F8E075Dc407e64080EA08372";
   // Wagmi hooks
   const account = useAccount();
   const { signMessage } = useSignMessage();
-  const result = useReadContract({
-    abi,
-    address: contractAddress,
-    functionName: "highestBidder",
-  });
-  console.log(result);
+  const {
+    data: transactionHash,
+    writeContract,
+    isError,
+    isSuccess,
+  } = useWriteContract();
+
+  async function submit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    writeContract({
+      address: contractAddress,
+      abi,
+      functionName: "bid",
+      value: parseEther("0.0001"),
+    });
+  }
 
   return (
     <main className="min-h-screen p-10">
@@ -29,6 +42,23 @@ export default function Home() {
       >
         Sign message
       </button>
+      <form onSubmit={submit}>
+        <button type="submit" className="btn btn-primary text-white mt-4">
+          Bid
+        </button>
+
+        {isSuccess && (
+          <div className="text-green-500 mt-2">
+            Bid placed successfully! Transaction Hash: {transactionHash}
+          </div>
+        )}
+
+        {isError && (
+          <div className="text-red-500 mt-2">
+            Failed to place bid. Please try again.
+          </div>
+        )}
+      </form>
     </main>
   );
 }
